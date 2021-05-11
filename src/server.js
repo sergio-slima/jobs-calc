04ed6bd9ec83f606b0ca98cc1679ccc581f54fe4
unknown <sergio_silvalima@hotmail.com>
2021-05-11 17:56:13 -0300

    server js ajustando

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get('/', (request, response) => {
-    return response.send('OIE!')
+    return response.sendFile("/Sergio/Projetos/rocketseat/maratona/jobs-calc/index.html")
 })
 
 server.listen(3000, () => console.log('rodando'))
