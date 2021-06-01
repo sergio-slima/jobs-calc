b162377cfe5adf7900b14bffeb6269155412b16e
unknown <sergio_silvalima@hotmail.com>
2021-06-01 15:39:06 -0300

    server use encoded

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,7 @@ const profile = {
 
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
-routes.post('/job', (req, res) => { console.log('salvar dados') })
+routes.post('/job', (req, res) => { console.log(req.body) })
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
